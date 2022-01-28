3760dbea8558bf3a372ab20629cc0616506a78b6
Joao Balico <joaobalico12@hotmail.com>
2022-01-28 22:30:17 +0000

    Update player class

--- script.js
+++ script.js
@@ -2,5 +2,16 @@ const canvas = document.getElementById('canvas');
 ctx = canvas.getContext('2d');
 
 class Player {
-    
+    constructor () {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 100;
+        this.height = 100
+    }
+
+    draw() {
+        
+    }
 }
